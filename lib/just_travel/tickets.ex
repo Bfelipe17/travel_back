0a8aa8b3bababec30a5c3504bf28305cc0f17a0e
Bfelipe17 <bruno.felipe17@hotmail.com>
2023-09-04 20:21:49 -0300

    feat: create ticket context

---
+++ lib/just_travel/tickets.ex
@@ -0,0 +1,97 @@
+defmodule JustTravel.Tickets do
+  @moduledoc """
+  The Tickets context.
+  """
+
+  import Ecto.Query, warn: false
+  alias JustTravel.Repo
+
+  alias JustTravel.Tickets.Ticket
+
+  @doc """
+  Returns the list of tickets.
+
+  ## Examples
+
+      iex> list_tickets()
+      [%Ticket{}, ...]
+
+  """
+  def list_tickets do
+    Repo.all(Ticket)
+  end
+
+  @doc """
+  Gets a single ticket.
+
+  ## Examples
+
+      iex> get_ticket(123)
+      {:ok, %Ticket{}}
+
+      iex> get_ticket(456)
+      ** {:error, "Ticket not found"}
+
+  """
+  def get_ticket(id) do
+    case Repo.get(Ticket, id) do
+      nil ->
+        {:error, "Ticket not found"}
+
+      ticket ->
+        {:ok, ticket}
+    end
+  end
+
+  @doc """
+  Creates a ticket.
+
+  ## Examples
+
+      iex> create_ticket(%{field: value})
+      {:ok, %Ticket{}}
+
+      iex> create_ticket(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_ticket(attrs \\ %{}) do
+    %Ticket{}
+    |> Ticket.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a ticket.
+
+  ## Examples
+
+      iex> update_ticket(ticket, %{field: new_value})
+      {:ok, %Ticket{}}
+
+      iex> update_ticket(ticket, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_ticket(%Ticket{} = ticket, attrs) do
+    ticket
+    |> Ticket.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a ticket.
+
+  ## Examples
+
+      iex> delete_ticket(ticket)
+      {:ok, %Ticket{}}
+
+      iex> delete_ticket(ticket)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_ticket(%Ticket{} = ticket) do
+    Repo.delete(ticket)
+  end
+end
