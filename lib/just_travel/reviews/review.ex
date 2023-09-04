d42a17c13a91473ebda733d9db865ae135db6d54
Bfelipe17 <bruno.felipe17@hotmail.com>
2023-09-04 20:22:54 -0300

    feat: add relationship between tickets and review

--- lib/just_travel/reviews/review.ex
+++ lib/just_travel/reviews/review.ex
@@ -1,12 +1,14 @@
 defmodule JustTravel.Reviews.Review do
   use Ecto.Schema
   import Ecto.Changeset
+  alias JustTravel.Tickets.Ticket
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "reviews" do
     field :comment, :string
     field :score, :float
+    belongs_to :ticket, Ticket
 
     timestamps()
   end
@@ -14,8 +16,8 @@ defmodule JustTravel.Reviews.Review do
   @doc false
   def changeset(review, attrs) do
     review
-    |> cast(attrs, [:score, :comment])
-    |> validate_required([:score])
+    |> cast(attrs, [:score, :comment, :ticket_id])
+    |> validate_required([:score, :ticket_id])
     |> validate_number(:score, greater_than_or_equal_to: 0, less_than_or_equal_to: 10)
   end
 end
