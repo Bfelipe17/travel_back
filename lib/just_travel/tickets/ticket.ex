fb70b1cc8f4fd2595e31e7e1dbed002d08d0e75d
Bfelipe17 <bruno.felipe17@hotmail.com>
2023-09-04 22:14:36 -0300

    feat: add location to ticket

--- lib/just_travel/tickets/ticket.ex
+++ lib/just_travel/tickets/ticket.ex
@@ -2,6 +2,7 @@ defmodule JustTravel.Tickets.Ticket do
   use Ecto.Schema
   import Ecto.Changeset
   alias JustTravel.Reviews.Review
+  alias JustTravel.Locations.Location
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -10,6 +11,7 @@ defmodule JustTravel.Tickets.Ticket do
     field :date, :date
     field :description, :string
     field :price, :decimal
+    belongs_to :location, Location
     has_many :reviews, Review, on_delete: :delete_all
 
     timestamps()
@@ -18,8 +20,8 @@ defmodule JustTravel.Tickets.Ticket do
   @doc false
   def changeset(ticket, attrs) do
     ticket
-    |> cast(attrs, [:name, :description, :date, :price])
-    |> validate_required([:name, :description, :date, :price])
+    |> cast(attrs, [:name, :description, :date, :price, :location_id])
+    |> validate_required([:name, :description, :date, :price, :location_id])
     |> validate_number(:price, greater_than_or_equal_to: 0)
   end
 end
